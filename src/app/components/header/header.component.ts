ba28c25758d6772118647e300980778ac2c9a644
joao <joao.neto1883@gmail.com>
2024-04-21 19:09:09 -0300

    add: slides, ajuste header

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,14 @@
 import { NgOptimizedImage } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NgOptimizedImage],
+  imports: [NgOptimizedImage,FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  value: String = '' 
 }
---
+++ src/app/slide/slide.component.ts
@@ -0,0 +1,14 @@
+import { Component,CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+
+
+@Component({
+  selector: 'app-slide',
+  standalone: true,
+  imports: [],
+  templateUrl: './slide.component.html',
+  styleUrl: './slide.component.css',
+  schemas:[CUSTOM_ELEMENTS_SCHEMA]
+})
+export class SlideComponent {
+
+}
